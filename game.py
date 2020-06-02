2fca47c082b661189b7b0271c309015557375772
Ruslan Valiev <40303641+gogiasss@users.noreply.github.com>
2020-06-02 23:46:49 +0500

    add main file

---
+++ game.py
@@ -0,0 +1,28 @@
+import random
+
+tries = 0
+
+number = random.randint(1, 50)
+
+
+print('Угадай число от 1 до 50')
+
+while tries < 6:
+    guess = int(input(' Введи число: '))
+
+    tries+=1
+
+    if guess < number:
+        print('Слишком мало')
+
+    if guess > number:
+        print('Слишком много.')
+
+    if guess == number:
+        print(f'Ты угадал! Это была {tries} попытка!')
+        break
+
+    if guess != number and tries == 6:
+        print(f'Ты проиграл. Количество попыток исчерпано. Я загадал число: {number}')
+
+print('Игра закончена..')
