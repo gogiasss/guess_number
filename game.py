7ec11a5563749fa85295ea33c00cefbebf5e8ce8
Rus <valiev.ruslan@gmail.com>
2020-06-02 23:56:16 +0500

    change description

--- game.py
+++ game.py
@@ -4,27 +4,29 @@ import random
 
 tries = 0
 
+#Генерируем случайное число
 number = random.randint(1, 50)
 
 
-print('Угадай число от 1 до 50')
+print("""Привет! Я загадал число в диапазоне от 1 до 50.
+         Попробуй его угадать. У тебя всего 6 попыток.""")
 
 while tries < 6:
-    guess = int(input(' Введи число: '))
+    guess = int(input('Введи число: '))
 
     tries+=1
 
     if guess < number:
-        print('Слишком мало')
+        print('Число слишком маленькое')
 
     if guess > number:
-        print('Слишком много.')
+        print('Число слишком большое.')
 
     if guess == number:
         print(f'Ты угадал! Это была {tries} попытка!')
         break
 
     if guess != number and tries == 6:
-        print(f'Ты проиграл. Количество попыток исчерпано. Я загадал число: {number}')
+        print(f'Ты проиграл. Количество попыток исчерпано. А я загадал число: {number}')
 
 print('Игра закончена..')
